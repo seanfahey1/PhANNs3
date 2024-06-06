cab23e88302837ee10a324e3bd49fe2d153deda1
Sean <sfahey1745@sdsu.edu>
2024-06-06 09:47:03 -0700

    'zscore accidently recursive'

--- phanns/src/utils/calc_arrays.py
+++ phanns/src/utils/calc_arrays.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.stats import zscore
+from scipy.stats import zscore as zs
 
 
 def zscore(array):
@@ -9,7 +9,7 @@ def zscore(array):
     print(array.shape)
     mean_array = np.mean(array, axis=1)
     stddev_array = np.std(array, axis=1)
-    zscore_array = zscore(array, axis=1)
+    zscore_array = zs(array, axis=1)
     print(mean_array.shape)
     print(zscore_array.shape)
     return mean_array, stddev_array, zscore_array
